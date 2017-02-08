4ba2dffe8f598000bca412158cce8eb37bc62493
Subash Basnet <subashbasnet@Subashs-iMac.local>
2017-02-08 13:49:08 +0200

    creating time travel with a year generator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,9 +9,17 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   currentTime
+  newTime
 
   constructor(public navCtrl: NavController) {
     this.currentTime = new Date().getFullYear();
   }
 
+  buttonClicked () {
+    this.newTime = this.getRandomInt(1000,4000);
+  }
+
+  getRandomInt(min,max){
+    return Math.floor(Math.random() * (max-min+1)) + min;
+  }
 }
